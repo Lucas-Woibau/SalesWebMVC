c80b9dd3def7072ade2e35bb2c6c56f4a5d65565
Lucas Woibau <lucaswoibau7@gmail.com>
2024-03-29 17:04:34 -0300

    Implementing simple search

--- SalesWebMVC/Program.cs
+++ SalesWebMVC/Program.cs
@@ -29,10 +29,9 @@ namespace SalesWebMVC
 
             //Seeding the DB
             builder.Services.AddScoped<SeedingService>();
-
             builder.Services.AddScoped<SellerService>();
-
             builder.Services.AddScoped<DepartmentService>();
+            builder.Services.AddScoped<SalesRecordService>();
 
             // Add services to the container.
             builder.Services.AddControllersWithViews();
---
+++ SalesWebMVC/Services/SalesRecordService.cs
@@ -0,0 +1,36 @@
+﻿using Microsoft.EntityFrameworkCore;
+using SalesWebMVC.Data;
+using SalesWebMVC.Models;
+
+namespace SalesWebMVC.Services
+{
+	public class SalesRecordService
+	{
+		private readonly SalesWebMVCContext _context;
+
+		public SalesRecordService(SalesWebMVCContext context)
+		{
+			_context = context;
+		}
+
+		public async Task<List<SalesRecord>> FindByDateAsync(DateTime? minDate, DateTime? maxDate)
+		{
+			var result = from obj in _context.SalesRecord select obj;
+			if (minDate.HasValue)
+			{
+				result = result.Where(x => x.Date >= minDate.Value);
+			}
+			if (maxDate.HasValue)
+			{
+				result = result.Where(x => x.Date <= maxDate.Value);
+			}
+
+			return await result
+				.Include(x => x.Seller)
+				.Include(x => x.Seller.Department)
+				.OrderByDescending(x => x.Date)
+				.ToListAsync();
+		}
+	}
+
+}
